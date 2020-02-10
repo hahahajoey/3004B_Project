4d266cd0faed368062601b98d76639722d50101f
hahahajoey <shenchupeng0807@gmail.com>
2020-02-10 15:41:50 -0500

    background
    finish

--- Stone/Assets/Scripts/Camera Scripts/CameraFollow.cs
+++ Stone/Assets/Scripts/Camera Scripts/CameraFollow.cs
@@ -23,15 +23,15 @@ public class CameraFollow : MonoBehaviour
     {
         Vector3 camera_temp = transform.position;
 
-        if (target.position.x > 2880)
-        { camera_temp.x = 2880; }
-        else if (target.position.x < 960)
+        if (target.position.x > 960)
         { camera_temp.x = 960; }
+        else if (target.position.x < -960)
+        { camera_temp.x = -960; }
 
-        if (target.position.y > 1620)
-        { camera_temp.y = 1620; }
-        else if (target.position.y < 540)
+        if (target.position.y > 540)
         { camera_temp.y = 540; }
+        else if (target.position.y < -540)
+        { camera_temp.y = -540 }
 
         transform.position = camera_temp;
     }
