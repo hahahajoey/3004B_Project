2be2088ee9a019bf6f09affcc320b318d9384df1
Xiannanchen <59699499+Xiannanchen@users.noreply.github.com>
2020-02-21 21:12:12 -0500

    item pick up and drop

---
+++ Stone/Assets/Scripts/Player Scripts/Pickup.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    private Inventory inventory;
+    public GameObject item;
+    void Start()
+    {
+        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            for(int i=0; i < inventory.slots.Length; i++)
+            {
+                if(inventory.isFull[i] == false)
+                {
+                    inventory.isFull[i] = true;
+                    Instantiate(item, inventory.slots[i].transform, false);
+                    Destroy(gameObject);
+                    break;
+                }
+            }
+        }
+    }
+}
