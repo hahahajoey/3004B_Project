0cecda518f951978b65cabce3b146a73312393cb
hahahajoey <shenchupeng0807@gmail.com>
2020-03-01 17:28:49 -0500

    update
    1

--- Stone/Assets/Scripts/Scene Scripts/Quit.cs
+++ Stone/Assets/Scripts/Scene Scripts/Quit.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class Quit : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void quit_function() 
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Debug.Log("called quit");
+        Application.Quit();
     }
 }
