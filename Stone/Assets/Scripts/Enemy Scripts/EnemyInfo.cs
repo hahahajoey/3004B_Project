513d0d6810780492602a2f0eee389393609169c6
JunrenJiang <995123894@qq.com>
2020-03-12 11:06:12 -0400

    Update animation
    Attack and Shoot

---
+++ Stone/Assets/Scripts/Enemy Scripts/EnemyInfo.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyInfo : MonoBehaviour
+{
+    public int maxHealth = 100;
+    int currentHealth;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        //Player hurt animation
+
+        if (currentHealth == 0){
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Debug.Log("Enemy died!");
+        //Die animation
+
+        //Disable
+    }
+}
