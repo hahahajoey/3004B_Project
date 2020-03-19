ffb5c1dfd2b795e64c6dd29cf3d6473c6b88465d
Zac Zhuang <zz351030211@gmail.com>
2020-03-19 12:53:18 -0400

    update
    1

---
+++ Stone/Assets/Scripts/Player Scripts/Landing.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Landing : MonoBehaviour
+{
+    public PlayerControlScript player;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("¿¿¿");
+        }
+    }
+}
