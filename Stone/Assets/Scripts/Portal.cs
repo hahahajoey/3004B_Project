0dedfb18edf2447c4d906263b78135db6ded5fae
Zac Zhuang <zz351030211@gmail.com>
2020-03-19 14:30:59 -0400

    update
    portal and landing zone

---
+++ Stone/Assets/Scripts/Portal.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    public PlayerControlScript player;
+
+    public GameObject Teleport;
+    public GameObject Player;
+
+   
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == ("Player"))
+        {
+            Debug.Log("¿¿¿");
+        }
+    }
+}
